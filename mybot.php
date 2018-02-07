89b3ccbaf6bf7f9aec890f3bbf22ca0fddf0ae07
leewooji <36217330+leewooji@users.noreply.github.com>
2018-02-07 11:23:53 +0700

    Update mybot.php

--- mybot.php
+++ mybot.php
@@ -40,7 +40,7 @@ if (!is_null($events['events'])) {
 				// Build message to reply back
 				$messages = [
 					'type' => 'text',
-					'text' => 'I Got You'
+					'text' => 'IGotYou'
 				];
 			}
 			// Make a POST Request to Messaging API to reply to sender
